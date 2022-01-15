c8f0b45db7f8e9e092857b9f122d40419bafd926
David Burnett <davidburnett@Davids-Mac-mini.local>
2022-01-15 14:00:21 +0000

    Add opencl to ffmpeg build options

--- build/build-ffmpeg.sh
+++ build/build-ffmpeg.sh
@@ -54,7 +54,7 @@ configure_build () {
       --enable-libaom --enable-libopenh264 --enable-libx264 --enable-libx265 --enable-libvpx \
       --enable-libmp3lame --enable-libopus --enable-neon --enable-runtime-cpudetect \
       --enable-audiotoolbox --enable-videotoolbox --enable-libvorbis --enable-libsvtav1 \
-      --enable-libass --enable-lto
+      --enable-libass --enable-lto --enable-opencl
 
   checkStatus $? "configuration of ${SOFTWARE} failed"
 
